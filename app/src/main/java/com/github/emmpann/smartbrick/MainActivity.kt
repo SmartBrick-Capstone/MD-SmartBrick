5efd9d474d240167e729b891ea3195852f58ec80
emmpann <mhdepan@gmail.com>
2023-12-05 17:12:29 +0700

    implement on boarding

--- app/src/main/java/com/github/emmpann/smartbrick/MainActivity.kt
+++ app/src/main/java/com/github/emmpann/smartbrick/MainActivity.kt
@@ -2,6 +2,7 @@ package com.github.emmpann.smartbrick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.github.emmpann.smartbrick.databinding.ActivityMainBinding
@@ -13,8 +14,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        navController = (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
+        navController.navInflater.inflate(R.navigation.main_navigation).apply {
+            setStartDestination(R.id.onBoardingFragment)
+            navController.graph = this
+        }
     }
 }
