01f70d0fd3d8383b05f21aef6893a4eef2af9da7
emmpann <mhdepan@gmail.com>
2023-12-05 10:40:27 +0700

    add fragment container

--- app/src/main/java/com/github/emmpann/smartbrick/MainActivity.kt
+++ app/src/main/java/com/github/emmpann/smartbrick/MainActivity.kt
@@ -2,10 +2,19 @@ package com.github.emmpann.smartbrick
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import com.github.emmpann.smartbrick.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        navController = (supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment).navController
     }
 }
