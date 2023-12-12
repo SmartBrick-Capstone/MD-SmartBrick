ac7979d3f3a2f918d7430a6f939ef9b0090c9516
emmpann <mhdepan@gmail.com>
2023-12-12 13:21:02 +0700

    modify home page

--- app/src/main/java/com/github/emmpann/smartbrick/feature/home/HomeFragment.kt
+++ app/src/main/java/com/github/emmpann/smartbrick/feature/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.github.emmpann.smartbrick.R
 import com.github.emmpann.smartbrick.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
+    private val viewModel: HomeViewModel by viewModels()
     private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
@@ -19,11 +21,17 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-        // Inflate the layout for this fragment
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupObserver()
+    }
+
+    private fun setupObserver() {
+        viewModel.currentUsername.observe(viewLifecycleOwner) {
+            binding.tvName.text = String.format(getString(R.string.home_welcome), it)
+        }
     }
 }
--- app/src/main/java/com/github/emmpann/smartbrick/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/github/emmpann/smartbrick/feature/home/HomeViewModel.kt
@@ -1,6 +1,8 @@
 package com.github.emmpann.smartbrick.feature.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.github.emmpann.smartbrick.core.data.local.preference.PreferencesManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -9,5 +11,5 @@ import javax.inject.Inject
 class HomeViewModel @Inject constructor(
     private val preferencesManager: PreferencesManager
 ) : ViewModel() {
-
+    val currentUsername: LiveData<String> = preferencesManager.getName().asLiveData()
 }
