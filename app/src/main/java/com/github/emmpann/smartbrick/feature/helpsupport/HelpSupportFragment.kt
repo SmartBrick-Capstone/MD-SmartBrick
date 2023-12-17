23be854f4e0cfa8a9a083338c2a845aec82d319a
emmpann <mhdepan@gmail.com>
2023-12-17 15:15:15 +0700

    modify help support page

--- app/src/main/java/com/github/emmpann/smartbrick/feature/helpsupport/HelpSupportFragment.kt
+++ app/src/main/java/com/github/emmpann/smartbrick/feature/helpsupport/HelpSupportFragment.kt
@@ -5,13 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.github.emmpann.smartbrick.R
+import com.github.emmpann.smartbrick.databinding.FragmentHelpSupportBinding
+
 class HelpSupportFragment : Fragment() {
+
+    private lateinit var binding: FragmentHelpSupportBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help_support, container, false)
+        binding = FragmentHelpSupportBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
     }
 }
