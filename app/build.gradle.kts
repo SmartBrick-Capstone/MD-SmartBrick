7da0ffc1e218879c055df5b282f82aed0195fbab
emmpann <mhdepan@gmail.com>
2023-12-21 12:08:41 +0700

    remove unused library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,9 +90,6 @@ dependencies {
     //paging3
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
 
-    //viewpager2
-    implementation ("androidx.viewpager2:viewpager2:1.0.0")
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
